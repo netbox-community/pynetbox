a13096eb9ae1eb0306dad6ce53b904839ec3f431
Zach Moody <zmoody@digitalocean.com>
2017-08-29 15:16:17 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pynetbox',
-    version='2.0.4',
+    version='2.0.5',
     description='NetBox API client library',
     url='https://github.com/digitalocean/pynetbox',
     author='Zach Moody',
