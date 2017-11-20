b5d31d1ae59a01ccfbff74a9a8637ac6e0e03f03
Zach Moody <zmoody@digitalocean.com>
2017-11-20 13:28:08 -0600

    remove unused import.

--- pynetbox/lib/query.py
+++ pynetbox/lib/query.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 import json
-from six.moves.urllib.parse import urlparse, urlencode
+from six.moves.urllib.parse import urlencode
 
 import requests
 
