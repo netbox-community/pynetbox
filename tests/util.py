dfbb716033bf060c5b3f2d27bc09702b31087ddc
zmoody <zmoody@digitalocean.com>
2018-07-10 15:35:17 -0500

    Fixes #60
    Resolves issue with non-ascii characters being mangled because req.json() wasn't being used.

--- tests/util.py
+++ tests/util.py
@@ -1,10 +1,15 @@
+import json
+
+
 class Response:
     def __init__(self, fixture=None, status_code=200, ok=True):
         self.status_code = status_code
         self.content = self.load_fixture(fixture)
         self.ok = ok
-        self.text = self.content
 
     def load_fixture(self, path):
         with open("tests/fixtures/{}".format(path), 'r') as f:
             return f.read()
+
+    def json(self):
+        return json.loads(self.content)
