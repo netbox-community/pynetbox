4ab1f2ae7640dca3dc633aa10da424b82fb3d955
Ryan Addessi (raddessi) <raddessi@users.noreply.github.com>
2021-01-05 19:23:50 -0700

    First stab at a device type import

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -12,6 +12,28 @@ import atexit
 
 
 DOCKER_PROJECT_PREFIX = "pytest_pynetbox"
+DEVICETYPE_LIBRARY_OBJECTS = [
+    "A10/TH6430.yaml",
+    "APC/AP8868.yaml",
+    "Arista/DCS-7010T-48.yaml",
+    "Arista/DCS-7050TX3-48C8.yaml",
+    "Arista/DCS-7280CR3-32P4.yaml",
+    "Arista/DCS-7280SR-48C6.yaml",
+    "Dell/Networking_S4048-ON.yaml",
+    "Dell/PowerEdge_R640.yaml",
+    "Generic/48-port_copper_patch_panel.yaml",
+    "Generic/LC-48-port_fiber_patch_panel.yaml",
+    "Opengear/IM7248-2-DAC-US.yaml",
+]
+DEVICE_ROLE_NAMES = [
+    "Border Leaf Switch",
+    "Console Server",
+    "Leaf Switch",
+    "PDU",
+    "Patch Panel",
+    "Server",
+    "Spine Switch",
+]
 
 
 @pytest.fixture(scope="session")
@@ -22,6 +44,30 @@ def git_toplevel():
         .decode("utf-8")
         .splitlines()[0]
     )
+
+
+@pytest.fixture(scope="session")
+def devicetype_library_repo_dirpath(git_toplevel):
+    """Get the path to the devicetype-library repo we will use."""
+    repo_fpath = os.path.join(git_toplevel, ".devicetype-library")
+    if os.path.isdir(repo_fpath):
+        subp.check_call(
+            ["git", "fetch",], cwd=repo_fpath,
+        )
+        subp.check_call(
+            ["git", "pull",], cwd=repo_fpath,
+        )
+    else:
+        subp.check_call(
+            [
+                "git",
+                "clone",
+                "https://github.com/netbox-community/devicetype-library",
+                repo_fpath,
+            ]
+        )
+
+    return repo_fpath
 
 
 @pytest.fixture(scope="session")
@@ -201,9 +247,102 @@ def id_netbox_service(fixture_value):
     return "netbox %s" % fixture_value
 
 
+def populate_netbox_object_types(nb_api, devicetype_library_repo_dirpath):
+    """Load some object types in to a fresh instance of netbox.
+    
+    These objects will be used in tests.py
+    """
+    # collect and load the configs for each of the requested object models
+    device_type_models = []
+    for object_model_relfpath in DEVICETYPE_LIBRARY_OBJECTS:
+        device_type_models.append(
+            yaml.safe_load(
+                open(
+                    os.path.join(
+                        devicetype_library_repo_dirpath,
+                        "device-types",
+                        object_model_relfpath,
+                    ),
+                    "r",
+                ).read()
+            )
+        )
+
+    # create the manufacturers
+    manufacturer_names = {model["manufacturer"] for model in device_type_models}
+    for manufacturer_name in manufacturer_names:
+        nb_api.dcim.manufacturers.create(
+            name=manufacturer_name, slug=manufacturer_name.lower().replace(" ", "-")
+        )
+
+    # create the device types and their components
+    for device_type_model in device_type_models:
+        device_type_model["manufacturer"] = {"name": device_type_model["manufacturer"]}
+        device_type = nb_api.dcim.device_types.create(**device_type_model)
+
+        for interface_template in device_type_model.get("interfaces", []):
+            nb_api.dcim.interface_templates.create(
+                device_type=device_type.id, **interface_template
+            )
+
+        for front_port_template in device_type_model.get("front_ports", []):
+            nb_api.dcim.front_port_templates.create(
+                device_type=device_type.id, **front_port_template
+            )
+
+        for rear_port_template in device_type_model.get("rear_ports", []):
+            nb_api.dcim.rear_port_templates.create(
+                device_type=device_type.id, **rear_port_template
+            )
+
+        for console_port_template in device_type_model.get("console-ports", []):
+            nb_api.dcim.console_port_templates.create(
+                device_type=device_type.id, **console_port_template
+            )
+
+        for console_server_port_template in device_type_model.get(
+            "console-server-ports", []
+        ):
+            nb_api.dcim.console_server_port_templates.create(
+                device_type=device_type.id, **console_server_port_template
+            )
+
+        for power_port_template in device_type_model.get("power-ports", []):
+            nb_api.dcim.power_port_templates.create(
+                device_type=device_type.id, **power_port_template
+            )
+
+        for power_outlet_template in device_type_model.get("power-outlets", []):
+            if "power_port" in power_outlet_template:
+                power_outlet_template["power_port"] = {
+                    "name": power_outlet_template["power_port"]
+                }
+            nb_api.dcim.power_outlet_templates.create(
+                device_type=device_type.id, **power_outlet_template
+            )
+
+        for device_bay_template in device_type_model.get("device-bays", []):
+            nb_api.dcim.device_bay_templates.create(
+                device_type=device_type.id, **device_bay_template
+            )
+
+    # add device roles
+    for device_role_name in DEVICE_ROLE_NAMES:
+        nb_api.dcim.device_roles.create(
+            name=device_role_name, slug=device_role_name.lower().replace(" ", "-")
+        )
+
+
 @pytest.fixture(scope="session")
-def netbox_service(pytestconfig, docker_ip, docker_services, request):
-    """Ensure that HTTP service is up and responsive."""
+def netbox_service(
+    pytestconfig, docker_ip, docker_services, devicetype_library_repo_dirpath, request
+):
+    """Get the netbox service to test against.
+    
+    This function waits until the netbox container is fully up and running then does an
+    initial data population with a few object types to be used in testing. Then the
+    service is returned as a fixture to be called from tests. 
+    """
     netbox_integration_version = request.param
 
     # `port_for` takes a container port and returns the corresponding host port
@@ -214,10 +353,15 @@ def netbox_service(pytestconfig, docker_ip, docker_services, request):
     docker_services.wait_until_responsive(
         timeout=300.0, pause=1, check=lambda: netbox_is_responsive(url)
     )
+    nb_api = pynetbox.api(url, token="0123456789abcdef0123456789abcdef01234567")
+    populate_netbox_object_types(
+        nb_api=nb_api, devicetype_library_repo_dirpath=devicetype_library_repo_dirpath
+    )
+
     return {
         "url": url,
         "netbox_version": netbox_integration_version,
-        "api": pynetbox.api(url, token="0123456789abcdef0123456789abcdef01234567"),
+        "api": nb_api,
     }
 
 
@@ -250,3 +394,15 @@ def docker_cleanup(pytestconfig):
     command_args = "version"
 
     return command_args
+
+
+@pytest.fixture(scope="session")
+def faker():
+    """Hook to let us set a consistent seed for the Faker module."""
+    # return 12345
+    import faker
+
+    fake = faker.Faker()
+    fake.seed_instance(0)
+
+    return fake
---
+++ tests/integration/test_dcim.py
@@ -0,0 +1,129 @@
+import pytest
+
+
+# @pytest.mark.usefixtures("netbox_service")
+class TestSimpleServerRackingAndConnecting:
+
+    """Verify we can create, rack, and connect a server."""
+
+    @pytest.fixture
+    def site(self, netbox_service, faker):
+        """Verify we can create a site."""
+        site_name = faker.city()
+        site = netbox_service["api"].dcim.sites.create(
+            name=site_name, slug=site_name.lower().replace(" ", "_")
+        )
+        assert site
+
+        return site
+
+        # # cleanup
+        # site.delete()
+        # assert not netbox_service["api"].dcim.sites.get(site_id)
+
+    @pytest.fixture
+    def rack(self, site, faker):
+        """Verify we can create a rack device."""
+        rack = site.api.dcim.racks.create(name="rack1", site=site.id)
+        assert rack
+
+        return rack
+
+        # # cleanup
+        # rack.delete()
+        # assert not site.api.dcim.racks.get(rack_id)
+
+    @pytest.fixture
+    def data_leafs(self, rack, faker):
+        """Verify we can create data leaf switch devices."""
+        devices = []
+        for i in range(1, 3):
+            device = rack.api.dcim.devices.create(
+                name="access_switch%s." % i + faker.domain_name(),
+                device_type={"slug": "dcs-7050tx3-48c8"},
+                device_role={"name": "Leaf Switch"},
+                site=rack.site.id,
+                rack=rack.id,
+                face="rear",
+                position=rack.u_height - i,
+            )
+            assert device
+            devices.append(device)
+
+        return devices
+
+    @pytest.fixture
+    def mgmt_leaf(self, rack, faker):
+        """Verify we can create data leaf switch devices."""
+        device = rack.api.dcim.devices.create(
+            name="mgmt_switch1." + faker.domain_name(),
+            device_type={"slug": "dcs-7010t-48"},
+            device_role={"name": "Leaf Switch"},
+            site=rack.site.id,
+            rack=rack.id,
+            face="rear",
+            position=rack.u_height - 3,
+        )
+        assert device
+
+        return device
+
+    @pytest.fixture
+    def server(self, site, faker):
+        """Verify we can create a server device."""
+        device = site.api.dcim.devices.create(
+            name=faker.hostname(),
+            device_type={"slug": "dell_poweredge_r640"},
+            device_role={"name": "Server"},
+            site=site.id,
+        )
+        assert device
+        return device
+
+    def test_racking_server(self, server, data_leafs, mgmt_leaf, rack):
+        """Verify we can rack the server."""
+        assert server.update(
+            {"rack": rack.id, "face": "front", "position": rack.u_height - 4}
+        )
+
+        # connect the mgmt iface
+        server_mgmt_iface = server.api.dcim.interfaces.get(
+            device_id=server.id, mgmt_only=True, cabled=False
+        )
+        assert server_mgmt_iface
+
+        mleaf_iface = mgmt_leaf.api.dcim.interfaces.filter(
+            mgmt_only=False, cabled=False
+        )[0]
+        cable = server.api.dcim.cables.create(
+            termination_a_type="dcim.interface",
+            termination_a_id=server_mgmt_iface.id,
+            termination_b_type="dcim.interface",
+            termination_b_id=mleaf_iface.id,
+        )
+
+        # connect the data ifaces in a bond
+        server_data_ifaces = server.api.dcim.interfaces.filter(
+            device_id=server.id, mgmt_only=False
+        )[:2]
+        assert len(server_data_ifaces) == 2
+
+        bond_iface = server.api.dcim.interfaces.create(
+            device=server.id, name="bond0", type="lag"
+        )
+        assert bond_iface
+
+        for server_data_iface, data_leaf in zip(server_data_ifaces, data_leafs):
+            dleaf_iface = data_leaf.api.dcim.interfaces.filter(
+                device_id=data_leaf.id, mgmt_only=False, cabled=False
+            )[0]
+            cable = server.api.dcim.cables.create(
+                termination_a_type="dcim.interface",
+                termination_a_id=server_data_iface.id,
+                termination_b_type="dcim.interface",
+                termination_b_id=dleaf_iface.id,
+            )
+            assert cable
+
+            assert server_data_iface.update({"lag": bond_iface.id})
+
