c4830aabe2a27c4564e9d37773fac289cae46600
Ryan Addessi (raddessi) <raddessi@users.noreply.github.com>
2021-02-09 18:38:15 -0700

    Re blacked with old black

--- pynetbox/models/dcim.py
+++ pynetbox/models/dcim.py
@@ -50,9 +50,9 @@ class TraceableRecord(Record):
                 #     https://netbox/api/dcim/rear-ports/12761/
                 # TODO: Move this to a more general function.
                 app_endpoint = "/".join(
-                    urlsplit(hop_item_data["url"]).path[len(urlsplit(self.api.base_url).path) :].split(
-                        "/"
-                    )[1:3]
+                    urlsplit(hop_item_data["url"])
+                    .path[len(urlsplit(self.api.base_url).path) :]
+                    .split("/")[1:3]
                 )
                 return_obj_class = uri_to_obj_class_map.get(app_endpoint, Record,)
                 this_hop_ret.append(
