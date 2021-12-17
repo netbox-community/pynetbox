75cc66b234e688a2c1661851dc7f1f6f5b78d470
Markku Leiniö <markkuleinio@users.noreply.github.com>
2021-12-17 20:07:56 +0200

    Set ObjectChanges.postchange_data and .prechange_data to JsonField

--- pynetbox/models/extras.py
+++ pynetbox/models/extras.py
@@ -22,6 +22,8 @@ class ConfigContexts(Record):
 
 class ObjectChanges(Record):
     object_data = JsonField
+    postchange_data = JsonField
+    prechange_data = JsonField
 
     def __str__(self):
         return self.request_id
