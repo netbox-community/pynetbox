0749a2dcb428998a7d7e579f2e59c7c12e644e67
Markku Leiniö <markkuleinio@users.noreply.github.com>
2022-03-17 18:11:55 +0200

    Remove threading warnings

--- pynetbox/core/query.py
+++ pynetbox/core/query.py
@@ -13,10 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-try:
-    import concurrent.futures as cf
-except ImportError:
-    pass
+import concurrent.futures as cf
 import json
 from urllib.parse import urlencode
 
