1f2cc83173731fc17c1ebff6e3f49c2027a4f349
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-08-25 01:06:23 +0530

    lint fixes

--- pynetbox/__init__.py
+++ pynetbox/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import metadata
 from pynetbox.core.query import RequestError, AllocationError, ContentError
 from pynetbox.core.api import Api as api
 
-__version__ = metadata(__name__).get('Version')
+__version__ = metadata(__name__).get("Version")
