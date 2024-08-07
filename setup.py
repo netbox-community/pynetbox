6653bf4aef4b5352eae293471b1161bd95b4a50e
Arthur Hanson <worldnomad@gmail.com>
2024-08-07 20:29:36 +0700

    update testing matrix, python version

--- setup.py
+++ setup.py
@@ -11,12 +11,9 @@ setup(
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     packages=find_packages(exclude=["tests", "tests.*"]),
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "requests>=2.20.0,<3.0",
-        "packaging<24.0"
-    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["requests>=2.20.0,<3.0", "packaging<24.0"],
     zip_safe=False,
     keywords=["netbox"],
     classifiers=[
