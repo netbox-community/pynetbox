2f5de9f17bdc0a22f50b3abd723ccc70f2cd446f
Arthur <worldnomad@gmail.com>
2025-12-19 15:24:07 -0800

    make base class for integration tests

--- tests/integration/test_dcim.py
+++ tests/integration/test_dcim.py
@@ -33,57 +33,11 @@ def device(api, site, device_type, role):
     device.delete()
 
 
-@pytest.mark.usefixtures("init")
-class BaseTest:
+from .base import BaseIntegrationTest
+
+
+class BaseTest(BaseIntegrationTest):
     app = "dcim"
-
-    def _init_helper(
-        self,
-        request,
-        fixture,
-        update_field=None,
-        filter_kwargs=None,
-        endpoint=None,
-        str_repr=None,
-    ):
-        request.cls.endpoint = endpoint
-        request.cls.fixture = fixture
-        request.cls.update_field = update_field
-        request.cls.filter_kwargs = filter_kwargs
-        request.cls.str_repr = str_repr
-
-    def test_create(self):
-        assert self.fixture
-
-    def test_str(self):
-        if self.str_repr:
-            test = str(self.fixture)
-            assert test == self.str_repr
-
-    def test_update_fixture(self):
-        if self.update_field:
-            setattr(self.fixture, self.update_field, "Test Value")
-            assert self.fixture.save()
-
-    def test_get_fixture_by_id(self, api):
-        test = getattr(getattr(api, self.app), self.endpoint).get(self.fixture.id)
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
-
-    def test_get_fixture_by_kwarg(self, api):
-        test = getattr(getattr(api, self.app), self.endpoint).get(**self.filter_kwargs)
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
-
-    def test_filter_fixture(self, api):
-        test = list(
-            getattr(getattr(api, self.app), self.endpoint).filter(**self.filter_kwargs)
-        )[0]
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
 
 
 class TestSite(BaseTest):
--- tests/integration/test_extras.py
+++ tests/integration/test_extras.py
@@ -1,57 +1,10 @@
 import pytest
 
+from .base import BaseIntegrationTest
 
-@pytest.mark.usefixtures("init")
-class BaseTest:
+
+class BaseTest(BaseIntegrationTest):
     app = "extras"
-
-    def _init_helper(
-        self,
-        request,
-        fixture,
-        update_field=None,
-        filter_kwargs=None,
-        endpoint=None,
-        str_repr=None,
-    ):
-        request.cls.endpoint = endpoint
-        request.cls.fixture = fixture
-        request.cls.update_field = update_field
-        request.cls.filter_kwargs = filter_kwargs
-        request.cls.str_repr = str_repr
-
-    def test_create(self):
-        assert self.fixture
-
-    def test_str(self):
-        if self.str_repr:
-            test = str(self.fixture)
-            assert test == self.str_repr
-
-    def test_update_fixture(self):
-        if self.update_field:
-            setattr(self.fixture, self.update_field, "Test Value")
-            assert self.fixture.save()
-
-    def test_get_fixture_by_id(self, api):
-        test = getattr(getattr(api, self.app), self.endpoint).get(self.fixture.id)
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
-
-    def test_get_fixture_by_kwarg(self, api):
-        test = getattr(getattr(api, self.app), self.endpoint).get(**self.filter_kwargs)
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
-
-    def test_filter_fixture(self, api):
-        test = list(
-            getattr(getattr(api, self.app), self.endpoint).filter(**self.filter_kwargs)
-        )[0]
-        assert test
-        if self.update_field:
-            assert getattr(test, self.update_field) == "Test Value"
 
 
 class TestTag(BaseTest):
